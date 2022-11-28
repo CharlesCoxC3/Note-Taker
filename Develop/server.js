2562df236f2497d1002dacb378d047b36021c55c
CharlesCoxC3 <charles.cox.iii@aol.com>
2022-11-27 20:48:48 -0800

    made new routes js and server js

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
