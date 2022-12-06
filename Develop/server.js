362853bc4a823a86fa817f8262082a5922b8a90d
CharlesCoxC3 <charles.cox.iii@aol.com>
2022-12-05 17:50:38 -0800

    reuploading code

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const PORT = 3001;
+
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require('./routes/api')(app);
+require('./routes/html')(app);
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server listening at localhost${PORT}`);
+});
